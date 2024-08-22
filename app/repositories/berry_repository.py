007bfaa22838c0781ab9abc1cb01e9eaddf64a1c
Kenjhy <kenjhyx@hotmail.com>
2024-08-22 12:08:01 -0500

    add loogs adn env to code

--- app/repositories/berry_repository.py
+++ app/repositories/berry_repository.py
@@ -11,13 +11,19 @@ logger = logging.getLogger(__name__)
 
 class BerryRepository:
     def __init__(self):
+        logger.info(f"CACHE_MAXSIZE: {os.getenv('CACHE_MAXSIZE')}")
+        logger.info(f"CACHE_TTL: {os.getenv('CACHE_TTL')}")
         self.base_url = f"{os.getenv('POKEAPI_BASE_URL', 'https://pokeapi.co/api/v2')}{os.getenv('BERRY_ENDPOINT', '/berry')}"
         self.cache = TTLCache(
             maxsize=int(os.getenv('CACHE_MAXSIZE', '1')),
             ttl=int(os.getenv('CACHE_TTL', '3600'))
         )
 
-    @cached(cache=TTLCache(maxsize=1, ttl=3600))
+
+    @cached(cache=TTLCache(
+        maxsize=int(os.getenv('CACHE_MAXSIZE', '1')),
+        ttl=int(os.getenv('CACHE_TTL', '3600'))
+    ))
     def get_all_berries(self) -> List[Berry]:
         berries = []
         next_url = self.base_url
